b35f96731d0a2df5d5d0d5a83548bbfee395c1ec
YouSayLari <larissa@think4.com.br>
2023-05-11 15:57:46 -0300

    Created UserAuthenticationContext

---
+++ UserAuthentication/Data/Context/UserAuthenticationContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace UserAuthentication.Data.Context
+{
+    public class UserAuthenticationContext : DbContext
+    {
+        public UserAuthenticationContext(DbContextOptions<UserAuthenticationContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+    }
+}

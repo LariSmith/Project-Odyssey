053564f4c45a91dd38cae9ca41a4d8eba556bb4b
YouSayLari <larissa@think4.com.br>
2023-05-13 14:49:33 -0300

    Created UserMap and added DbSet Users

---
+++ UserAuthentication/Data/Context/Maps/UsersMap.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using UserAuthentication.Domain.Entities;
+
+namespace UserAuthentication.Data.Context.Maps
+{
+    public class UsersMap : IEntityTypeConfiguration<Users>
+    {
+        public void Configure(EntityTypeBuilder<Users> builder)
+        {
+
+        }
+    }
+}
--- UserAuthentication/Data/Context/UserAuthenticationContext.cs
+++ UserAuthentication/Data/Context/UserAuthenticationContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
+using UserAuthentication.Domain.Entities;
 
 namespace UserAuthentication.Data.Context
 {
@@ -8,6 +9,8 @@ namespace UserAuthentication.Data.Context
         public UserAuthenticationContext(DbContextOptions<UserAuthenticationContext> options) : base(options)
         {
         }
+
+        public DbSet<Users> Users { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {

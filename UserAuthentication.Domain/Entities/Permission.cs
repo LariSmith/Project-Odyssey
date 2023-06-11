ccce4e351ebe25ae5c3a720a3249b8f4f4c3a918
YouSayLari <larissa@think4.com.br>
2023-06-11 00:00:19 -0300

    Moving entities and value objects to new library

---
+++ UserAuthentication.Domain/Entities/Permission.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserAuthentication.Domain.Entities
+{
+    public class Permission
+    {
+        [Key]
+        public Guid Id { get; private set; }
+
+        public string PermissionName { get; private set; }
+
+        public ICollection<RolePermission> RolePermissions { get; internal set; } = new List<RolePermission>();
+
+        public Permission(Guid id, string permissionName)
+        {
+            Id = id;
+            PermissionName = permissionName;
+        }
+
+    }
+}
---
+++ UserAuthentication.Domain/Entities/User.cs
@@ -0,0 +1,85 @@
+﻿using System.ComponentModel.DataAnnotations;
+using UserAuthentication.Domain.ValuesObjects;
+
+namespace UserAuthentication.Domain.Entities
+{
+    public class User
+    {
+        [Key]
+        public Guid Id { get; private set; }
+
+        public Username Username { get; private set; }
+
+        public Email Email { get; private set; }
+
+        public string PasswordHash { get; private set; }
+
+        public string Name { get; private set; }
+
+        public DateTime CreateAt { get; private set; }
+
+        public DateTime UpdateAt { get; private set; }
+
+        public IReadOnlyCollection<UserRole> Roles => _roles;
+        private readonly HashSet<UserRole> _roles = new HashSet<UserRole>();
+
+        private RefreshToken? _refreshToken { get; set; }
+
+        public User(Username username, Email email, string passwordHash, string name)
+        {
+            Id = Guid.NewGuid();
+            Username = username;
+            Email = email;
+            PasswordHash = passwordHash;
+            Name = name;
+            CreateAt = DateTime.UtcNow;
+            UpdateAt = DateTime.UtcNow;
+        }
+
+        private User() { }
+
+        public void AddRole(Role role)
+        {
+            var user = new User
+            {
+                Id = Id,
+                Username = Username,
+                Email = Email,
+                PasswordHash = PasswordHash,
+                Name = Name,
+                CreateAt = CreateAt,
+                UpdateAt = UpdateAt
+            };
+
+            var userRoles = new UserRole(Guid.NewGuid(), role, user);
+            _roles.Add(userRoles);
+            role.UserRoles.Add(userRoles);
+        }
+
+        public void CreateRefreshToken(string token, DateTime expirationTime)
+        {
+            var user = new User
+            {
+                Id = Id,
+                Username = Username,
+                Email = Email,
+                PasswordHash = PasswordHash,
+                Name = Name,
+                CreateAt = CreateAt,
+                UpdateAt = UpdateAt
+            };
+
+            var refreshToken = new RefreshToken(Guid.NewGuid(), user, token, expirationTime);
+            _refreshToken = refreshToken;
+        }
+
+        public RefreshToken? GetRefreshToken()
+        {
+            if (_refreshToken == null)
+                return null;
+
+            return _refreshToken;
+        }
+
+    }
+}

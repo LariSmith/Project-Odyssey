e72a370c22993f7875ec0071c51bb48b4734db92
YouSayLari <larissa@think4.com.br>
2023-06-11 17:13:54 -0300

    Creating BaseRepository in the infrastructure layer

---
+++ UserAuthentication.Infraestructure/Repositories/BaseRepository.cs
@@ -0,0 +1,38 @@
+﻿using UserAuthentication.Domain.Abstractions;
+using UserAuthentication.Domain.Primitives;
+using UserAuthentication.Presentation;
+
+namespace UserAuthentication.Infraestructure.Repositories
+{
+    public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : BaseEntity
+    {
+        protected readonly UserAuthenticationContext _context;
+
+        public BaseRepository(UserAuthenticationContext context)
+        {
+            _context = context;
+        }
+
+        public void Insert(TEntity obj)
+        {
+            _context.Set<TEntity>().Add(obj);
+            _context.SaveChanges();
+        }
+
+        public void Update(TEntity obj)
+        {
+            _context.Entry(obj).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            _context.Set<TEntity>().Remove(Select(id));
+            _context.SaveChanges();
+        }        
+
+        public IList<TEntity> Select() => _context.Set<TEntity>().ToList();
+
+        public TEntity Select(int id) => _context.Set<TEntity>().Find(id);
+    }
+}
